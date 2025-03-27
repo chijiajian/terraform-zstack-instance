d24f3e9633eeb8c3dcde940951a6de5b170e0eaa
jiajian.chi <jiajian.chi@zstack.io>
2025-03-27 13:37:36 +0800

    feat: add instance module

---
+++ examples/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    zstack = {
+      source  = "ZStack-Robot/zstack"
+      version = "1.0.5"
+    }
+  }
+}
+
+provider "zstack" {
+  host             = "1.30.3.3"   # ZStack 管理节点的 IP
+  account_name     = "admin"         # 账户名称
+  account_password = "xxxx"      # 账户密码
+}
+
+
+# 获取镜像
+module "zstack_image" {
+  source = "git::http://dev.zstack.io:9080/jiajian.chi/terraform-zstack-image.git"
+
+  providers = {
+    zstack = zstack
+  }
+
+  create_image = false
+  image_name   = "Ubuntu20.04_base"  # 指定镜像名称
+}
+
+# 创建虚拟机实例
+module "zstack_instance" {
+  source = "../"  
+
+  name = "basic-instance"
+  description           = "basic instance created by Terraform"
+  instance_count        = 1
+  image_uuid             = module.zstack_image.image_uuid  
+
+  # 通过名称查询资源
+  l3_network_name       = "test"
+  instance_offering_name = "min"
+}
+
+output "instance_ids" {
+  value = module.zstack_instance.instances[*].uuid  
+}
+
+output "instance_names" {
+  value = module.zstack_instance.instances[*].name  
+}
+
+output "instance_ip_addresses" {
+  value = module.zstack_instance.instance_ips
+}
+
+# 访问特定实例的IP地址
+output "first_instance_ip" {
+  value = module.zstack_instance.instance_ips[0]
+}
--- main.tf
+++ main.tf
@@ -1,9 +1,47 @@
+terraform {
+  required_providers {
+    zstack = {
+      source  = "zstack-robot/zstack"
+      version = "1.0.5"
+    }
+  }
+}
+
 locals {
   context = var.context
 }
 
-module "submodule" {
-  source = "./modules/submodule"
 
-  message = "Hello, submodule"
+
+data "zstack_l3networks" "l3networks" {
+  count = var.l3_network_name != null ? 1 : 0
+  name  = var.l3_network_name
+}
+
+# 如果提供了计算规格名称，则查询计算规格
+data "zstack_instance_offers" "offers" {
+  count = var.instance_offering_name != null ? 1 : 0
+  name  = var.instance_offering_name
+}
+
+locals {
+  # 确保l3_network_uuids不为空
+  l3_network_uuids = var.l3_network_name != "" ? [data.zstack_l3networks.l3networks[0].l3networks[0].uuid] : var.l3_network_uuids
+  
+  # 确保instance_offering_uuid不为空
+  instance_offering_uuid = var.instance_offering_uuid != "" ? var.instance_offering_uuid : (
+    try(data.zstack_instance_offers.offers[0].instance_offers[0].uuid, "")
+  )
+  
+}
+
+
+resource "zstack_instance" "instance" {
+  count = var.instance_count
+
+  name                   = var.instance_count > 1 ? "${var.name}-${count.index + 1}" : var.name
+  description            = var.description
+  image_uuid             = var.image_uuid
+  instance_offering_uuid = local.instance_offering_uuid != "" ? local.instance_offering_uuid : null
+  l3_network_uuids           = local.l3_network_uuids
 }
